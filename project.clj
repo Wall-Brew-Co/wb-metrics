cd7a663a2f6a087561c96479641d91f4f672a55c
Nick Nichols <nick.nichols@rate.com>
2020-05-01 08:45:05 -0500

    Bump external deps

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/Wall-Brew-Co/wb-metrics"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[com.fzakaria/slf4j-timbre "0.3.13"]
+  :dependencies [[com.fzakaria/slf4j-timbre "0.3.19"]
                  [com.taoensso/timbre "4.10.0"]
                  [metrics-clojure "2.10.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [robert/hooke "1.3.0"]])
